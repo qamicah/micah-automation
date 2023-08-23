0be167b16ed17540f74d0ebcb939cf74bfe89336
micahjang <urmicah@gmail.com>
2023-08-23 21:09:00 +0900

    오더 케이스 추가

--- src/st1/Order_test.java
+++ src/st1/Order_test.java
@@ -1,23 +1,14 @@
-package st1;
-
-import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class test_result {
-
+public class Order_test {
     private static WebDriver driver;
 
     @Before
@@ -27,7 +18,7 @@ public class test_result {
         ops.addArguments("--remote-allow-origins=*");
 
 
-        System.setProperty("webdriver.chrome.driver", "/Users/micahjang/Documents/selenium/chromedriver");
+        System.setProperty("Webdriver.chrome.driver", "/Users/mk-am14-014/Documents/selenium/chromedriver");
         driver = new ChromeDriver(ops);
 
 
@@ -37,13 +28,13 @@ public class test_result {
         // 최대 10초 동안 대기
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
-        // 컬리 접속
-        driver.get("https://www.kurly.com/member/login");
+        // stg 접속
+        driver.get("https://www.stg.kurly.com/member/login");
 
     }
 
     @Test
-    public void Test() throws InterruptedException {
+    public void login() throws InterruptedException {
 
         // 10초 동안 대기
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -60,21 +51,31 @@ public class test_result {
         WebElement login_click = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"__next\"]/div[3]/div[2]/form/div[3]/button[1]")));
         login_click.click();
 
-        // 마이컬리 클릭
-        WebElement mykurly = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"__next\"]/div[2]/div[1]/div[1]/div[1]/a/span[1]")));
-        mykurly.click();
-    }
-
-    @After
-    public void After() throws InterruptedException {
-
-        // 결과확인
+        // 최대 10초 동안 대기
         Thread.sleep(2000);
-        Assert.assertEquals("장미가님", driver.findElement(By.xpath("//*[@id=\"__next\"]/div[2]/div[1]/div/div[1]/div[1]/div/strong")).getText());
-        System.out.println("test_result 참 잘했어요");
 
 
+        // 장바구니 진입
+        WebElement cart_in = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"__next\"]/div[2]/div[1]/div[2]/div[3]/div/div[2]/button")));
+        cart_in.click();
+        Thread.sleep(3000);
+
+        // 주문서 진입
+        WebElement order_bnt = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"__next\"]/div[3]/div[2]/div[2]/div/div[3]/button[1]")));
+        order_bnt.click();
+        Thread.sleep(3000);
+
+        // 페이지 스크롤
+        driver.findElement(By.cssSelector("body")).sendKeys(Keys.PAGE_DOWN);
 
 
+        //적립금 전체선택
+        WebElement point_bnt = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"__next\"]/div[2]/div/div/div[9]/div[1]/div[6]/div/div[1]/button")));
+        point_bnt.click();
+        Thread.sleep(3000);
+
+
+        //driver.quit();
     }
 }
+
